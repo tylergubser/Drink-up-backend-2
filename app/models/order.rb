e9e2591e95111d85ddbadf2e6f5278f9a97494bc
tylergubser <tylergubser2200@gmail.com>
2022-05-06 15:01:34 -0500

    added orders validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :drink
   belongs_to :user
+
+  validates :phone_number, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
