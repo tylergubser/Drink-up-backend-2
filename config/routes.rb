0d39333e4094759398e76dc09f53002b1e0e5ba9
tylergubser <tylergubser2200@gmail.com>
2022-05-06 10:59:26 -0500

    added twilio

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :menus, only: [:index, :create, :show]
   resources :drinks, only: [:index, :create, :destroy]
   resources :users, only: [:index, :create]
+  resources :sms_messages, only: [:create]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_03_193436) do
+ActiveRecord::Schema.define(version: 2022_05_06_151238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 2022_05_03_193436) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["drink_id"], name: "index_orders_on_drink_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "sms_messages", force: :cascade do |t|
+    t.string "mobile_number"
+    t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
